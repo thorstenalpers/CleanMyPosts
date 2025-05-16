bdef83497c81d6c014d5124934ace8142b1177fc
thorstenalpers <thorstenalpers@web.de>
2025-05-16 18:26:13 +0200

    Simplify dialog

--- src/UI/ViewModels/ShellDialogViewModel.cs
+++ src/UI/ViewModels/ShellDialogViewModel.cs
@@ -1,25 +1,15 @@
-﻿using System.Windows.Input;
-
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace XTweetCleaner.UI.ViewModels;
 
-public class ShellDialogViewModel : ObservableObject
+public partial class ShellDialogViewModel : ObservableObject
 {
-    private ICommand _closeCommand;
-
-    public ICommand CloseCommand => _closeCommand ?? (_closeCommand = new RelayCommand(OnClose));
-
     public Action<bool?> SetResult { get; set; }
 
-    public ShellDialogViewModel()
+    [RelayCommand]
+    private void Close()
     {
-    }
-
-    private void OnClose()
-    {
-        var result = true;
-        SetResult(result);
+        SetResult?.Invoke(true);
     }
 }

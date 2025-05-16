51aaee6ac4bfb3710d2f9322cd0106b09ac0187f
thorstenalpers <thorstenalpers@web.de>
2025-05-16 18:23:31 +0200

    Show progressbar and disable user interactions

---
+++ src/UI/Views/OverlayWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace XTweetCleaner.UI.Views;
+/// <summary>
+/// Interaction logic for OverlayWindow.xaml
+/// </summary>
+public partial class OverlayWindow : Window
+{
+    public OverlayWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            var mainWindow = this.Owner;
+            if (mainWindow != null)
+            {
+                try
+                {
+                    // Try dragging the main window instead
+                    ((Window)mainWindow).DragMove();
+                }
+                catch (InvalidOperationException)
+                {
+                    // DragMove can throw if mouse not down properly
+                }
+            }
+        }
+    }
+}

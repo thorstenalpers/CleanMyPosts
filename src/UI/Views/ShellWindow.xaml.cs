0ec9477c07f595460e5d5a6fe29235bb78b81245
thorstenalpers <thorstenalpers@web.de>
2025-05-23 18:48:21 +0200

    Fix window restore

--- src/UI/Views/ShellWindow.xaml.cs
+++ src/UI/Views/ShellWindow.xaml.cs
@@ -13,6 +13,8 @@ public partial class ShellWindow : MetroWindow, IShellWindow
     private readonly IUserSettingsService _userSettingsService;
     private bool _settingsLoaded = false;
 
+    private const double DefaultWidth = 860;
+    private const double DefaultHeight = 600;
     public ShellWindow(ShellViewModel viewModel, IUserSettingsService userSettingsService)
     {
         _userSettingsService = userSettingsService ?? throw new ArgumentNullException(nameof(userSettingsService));
@@ -23,22 +25,83 @@ public partial class ShellWindow : MetroWindow, IShellWindow
         Closing += OnClosing;
     }
 
+    private double GetCenteredLeft()
+    {
+        var screenWidth = SystemParameters.WorkArea.Width;
+        return (screenWidth - DefaultWidth) / 2;
+    }
+
+    private double GetCenteredTop()
+    {
+        var screenHeight = SystemParameters.WorkArea.Height;
+        return (screenHeight - DefaultHeight) / 2;
+    }
+
+    private WindowSettings _lastNormalBounds;
+
     private void OnLoaded(object sender, RoutedEventArgs e)
     {
         var settings = _userSettingsService.GetWindowSettings();
 
-        // Basic screen bounds check
-        if (settings.Left >= 0 && settings.Top >= 0)
+        if (settings.WindowState == WindowState.Maximized)
         {
-            Left = settings.Left;
-            Top = settings.Top;
+            Left = settings.Left >= 0 ? settings.Left : GetCenteredLeft();
+            Top = settings.Top >= 0 ? settings.Top : GetCenteredTop();
+            Width = settings.Width > 0 ? settings.Width : DefaultWidth;
+            Height = settings.Height > 0 ? settings.Height : DefaultHeight;
+
+            WindowState = WindowState.Maximized;
+        }
+        else
+        {
+            Left = settings.Left >= 0 ? settings.Left : GetCenteredLeft();
+            Top = settings.Top >= 0 ? settings.Top : GetCenteredTop();
+            Width = settings.Width > 0 ? settings.Width : DefaultWidth;
+            Height = settings.Height > 0 ? settings.Height : DefaultHeight;
+            WindowState = WindowState.Normal;
         }
 
-        Width = settings.Width;
-        Height = settings.Height;
-        WindowState = settings.WindowState;
+        _lastNormalBounds = new WindowSettings
+        {
+            Left = Left,
+            Top = Top,
+            Width = Width,
+            Height = Height,
+            WindowState = WindowState.Normal
+        };
 
         _settingsLoaded = true;
+        StateChanged += ShellWindow_StateChanged;
+    }
+
+
+    private void ShellWindow_StateChanged(object sender, EventArgs e)
+    {
+        if (!_settingsLoaded)
+        {
+            return;
+        }
+
+        if (WindowState == WindowState.Normal)
+        {
+            // User restored window from maximized or minimized -> reset to default size/position
+
+            var settings = _userSettingsService.GetWindowSettings();
+            Left = settings.Left >= 0 ? settings.Left : GetCenteredLeft();
+            Top = settings.Top >= 0 ? settings.Top : GetCenteredTop();
+            Width = DefaultWidth;
+            Height = DefaultHeight;
+
+            // Update _lastNormalBounds accordingly
+            _lastNormalBounds.Left = Left;
+            _lastNormalBounds.Top = Top;
+            _lastNormalBounds.Width = Width;
+            _lastNormalBounds.Height = Height;
+        }
+        else if (WindowState == WindowState.Maximized)
+        {
+            // Do nothing special here
+        }
     }
 
     private void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -48,17 +111,33 @@ public partial class ShellWindow : MetroWindow, IShellWindow
             return;
         }
 
-        var settings = new WindowSettings
+        var settings = new WindowSettings();
+
+        if (WindowState == WindowState.Maximized)
         {
-            Top = Top,
-            Left = Left,
-            Width = Width,
-            Height = Height,
-            WindowState = WindowState
-        };
+            settings.WindowState = WindowState.Maximized;
+
+            // Save the last known normal bounds, _not_ the maximized bounds
+            settings.Left = _lastNormalBounds.Left;
+            settings.Top = _lastNormalBounds.Top;
+            settings.Width = _lastNormalBounds.Width;
+            settings.Height = _lastNormalBounds.Height;
+        }
+        else
+        {
+            // Normal state, save current bounds as last known normal
+            settings.WindowState = WindowState.Normal;
+            settings.Left = Left;
+            settings.Top = Top;
+            settings.Width = Width;
+            settings.Height = Height;
+
+            _lastNormalBounds = settings;
+        }
 
         _userSettingsService.SaveWindowsSettings(settings);
     }
+
 
     public Frame GetNavigationFrame()
         => shellFrame;
